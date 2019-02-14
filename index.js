6cb9bcbbb08c4ea864125373182d1fec9abb59d2
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-14 01:53:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+class BoardMember(name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+
+  veto(){
+    returns "No, I must disagree"
+  }
+
+  approve() {
+    return "You can do that!"
+  }
+
+  doCharity() {
+    returns "I like to help people."
+  }
+
+  releasePressStatement() {
+    returns "You will see great things from Scuber."
+  }
+
+  sayHi() {
+    returns  `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training} `
+  }
+    
+}
